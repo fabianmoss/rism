5309cee369b4f8a1b68f971eecf0516a0eab954d
Fabian Moss <fabianmoss@gmail.com>
2026-01-19 10:50:14 +0100

    add query and read functions

---
+++ README.md
@@ -0,0 +1,3 @@
+# rism 
+
+Minimal Python package to query and print data from [RISM Online](https://rism.online).
--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from rism!")
+import requests
+import time
+from pprint import pprint as print
+
+BASE_URL = "http://rism.online"
+HEADERS = {"Accept": "application/ld+json"}
+
+
+def get_source(source_id):
+    url = f"{BASE_URL}/sources/{source_id}"
+    resp = requests.get(url, headers=HEADERS)
+    resp.raise_for_status()
+    return resp.json()
+
+
+def search_sources(q, mode="sources", rows=20):
+    params = {"q": q, "mode": mode, "rows": rows}
+    url = f"{BASE_URL}/search"
+    resp = requests.get(url, headers=HEADERS, params=params)
+    resp.raise_for_status()
+    return resp.json()
 
 
 if __name__ == "__main__":
-    main()
+    query = "Pietro Pontio"
+    print(f"Query: {query}")
+    time.sleep(1)
+
+    response = search_sources(query)
+    print(f"Found {len(response['items'])} entries.")
+    time.sleep(1)
+
+    first_entry_id = response["items"][0]["id"].split("/")[-1]
+    data = get_source(first_entry_id)
+    print(data)
